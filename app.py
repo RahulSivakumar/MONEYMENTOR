6115b586fc9b79d9886a59485575f6f970bef058
RahulSivakumar <rahulsivakumar9@gmail.com>
2026-03-30 00:29:09 +0530

    Added a greeting

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
+st.title("Money Mentor 💰")
+st.write("Helping you track your wealth, one step at a time.")
+
+# Let's add an input box
+name = st.text_input("What is your name?")
+if name:
+    st.write(f"Hello {name}, let's get your finances on track!")import streamlit as st
+
 st.title("Money Mentor")
 st.write("This is my financial app, built by me!")
